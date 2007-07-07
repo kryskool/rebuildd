fcb88ba34c39dcf9b9d0039f8e4a9435a954a0bf
Julien Danjou <acid@debian.org>
2007-07-07 14:18:20 +0000

    add setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2.5
+
+# Setup script for rebuildd
+
+from distutils.core import setup
+
+setup(name="rebuildd",
+      version="0.1.0",
+      description="Debian packages rebuild tool",
+      author="Julien Danjou",
+      author_email="acid@debian.org",
+      url="http://julien.danjou.info/rebuildd.html",
+      packages=['rebuildd'])
