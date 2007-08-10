1950151de42b8d6835b93ecc6f007c92e750e92c
Julien Danjou <acid@debian.org>
2007-08-10 23:50:40 +0000

    forgot to import with_statement

--- rebuildd/RebuilddHTTPServer.py
+++ rebuildd/RebuilddHTTPServer.py
@@ -16,6 +16,8 @@
 #   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #
 
+from __future__ import with_statement
+
 from RebuilddConfig import RebuilddConfig
 from Rebuildd import Rebuildd
 from Package import Package
