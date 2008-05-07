2555d0954faacfccb10f23e642e429780614f441
Julien Danjou <julien@danjou.info>
2008-05-07 13:49:24 +0200

    Switch to python2.5 which is now default
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- rebuildd.py
+++ rebuildd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.5
+#!/usr/bin/python
 #
 # rebuildd - Debian packages rebuild tool
 #
--- tests/TestJob.py
+++ tests/TestJob.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.5
+#!/usr/bin/python
 import sys  
   
 sys.path.insert(0, "..")  
