e54ee95629f1456848d3d56c5da4805d29c07203
Daniel Dehennin <daniel.dehennin@baby-gnu.org>
2012-01-20 10:58:08 +0100

    Upstream URL is now http://julien.danjou.info/software/rebuildd.html.
    * setup.py: Update url.
    
    * debian/control (Homepage): Update.
    
    Closes: #656580

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name="rebuildd",
       description="Debian packages rebuild tool",
       author="Julien Danjou",
       author_email="acid@debian.org",
-      url="http://julien.danjou.info/rebuildd.html",
+      url="http://julien.danjou.info/software/rebuildd.html",
       packages=['rebuildd'])
